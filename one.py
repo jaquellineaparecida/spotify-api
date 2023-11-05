d75ccb659ba922ce1d6f70a935286229a08b9f60
jaquelline <jaquellineaparecida04@gmail.com>
2023-11-05 20:25:39 -0300

    changes

--- one.py
+++ one.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import base64
 import requests
-import json
+import oracledb
 
 app = Flask(__name__)
 
@@ -52,7 +52,7 @@ def index():
 # listando os artistas 
 @app.route('/artists', methods=['GET'])
 def listando_artistas():
-    url = 'https://api.spotify.com/v1/artists?ids=2CIMQHirSU0MQqyYHq0eOx,57dN52uHvrHOxijzpIgu3E,1vCWHaC5f2uS3yhpwWbIA6,06HL4z0CvFAxyc27GXpf02,0EmeFodog0BfCgMzAIvKQp,6eUKZXaKkcviH0Ku9w2n3V,41MozSoPIsD1dJM0CLPjZF,0du5cEVh5yTK9QJze8zA0C,3Nrfpe0tUJi4K4DXYWgMUX,66CXWjxzNUsdJxJ2JdwvnR,6M2wZ9GZgrQXHCFfjv46we,4nDoRrQiYLoBzwC5BhVJzF,6S2OmqARrzebs0tKUEyXyp,7n2wHs1TKAczGzO7Dd2rGr,3zgnrYIltMkgeejmvMCnes'
+    url = 'https://api.spotify.com/v1/artists?ids=06HL4z0CvFAxyc27GXpf02,6eUKZXaKkcviH0Ku9w2n3V,41MozSoPIsD1dJM0CLPjZF,0du5cEVh5yTK9QJze8zA0C,66CXWjxzNUsdJxJ2JdwvnR,6M2wZ9GZgrQXHCFfjv46we'
 
     acesso = acessando_token()
     if acesso:
@@ -71,7 +71,7 @@ def listando_artistas():
 #listando albuns 
 @app.route('/albuns', methods=['GET'])
 def listando_albuns():
-    url = 'https://api.spotify.com/v1/albums/3zgnrYIltMkgeejmvMCnes'
+    url = 'https://api.spotify.com/v1/artists/06HL4z0CvFAxyc27GXpf02/albums'
 
     acesso = acessando_token()
     if acesso:
@@ -82,16 +82,16 @@ def listando_albuns():
         if response.status_code == 200:
             dados = response.json()
         else:
-            dados = []
+            dados = {'items': []}
         return render_template('albuns.html', albuns=dados)
     else:
-        return 'Deu erro smt'
+        return 'Deu erro'
 
 # listando top 5 musicas dos artistas 
 @app.route('/songs', methods=['GET'])
 def listando_top5_musicas():
 
-    url = f'https://api.spotify.com/v1/artists?ids=3zgnrYIltMkgeejmvMCnes/top-tracks'
+    url = 'https://api.spotify.com/v1/albums/151w1FgRZfnKZA9FEcg9Z3/tracks'
 
     acesso = acessando_token()
     if acesso:
@@ -102,10 +102,106 @@ def listando_top5_musicas():
         if response.status_code == 200:
             dados = response.json()
         else:
-            dados = []
+            dados = {'tracks': []}
         return render_template('songs.html', tracks=dados)
     else:
         return 'Deu erro smt'
+    
+
+# função para se conectar com banco de dados
+def getConnection():
+    try:
+        conn = oracledb.connect(user="rm99553", password="020904", host=1521, service_name= "ORCL")
+    except Exception as e:
+        print(f'Erro ao obter a conexão: {e}')
+    return conn
+
+#Execução de um select na tabela T_ARTIST
+def selectArtists():
+    try:
+        conn = getConnection()
+        cursor = conn.cursor()
+        sql_select = 'SELECT * FROM T_ARTIST'
+        cursor.execute(sql_select)
+        i = 1  
+        for result in cursor: 
+            print(f'\nlinha {i}: {result}')
+            i += 1
+    except Exception as e:
+        print(f'Erro ao executar select: {e}')
+
+#Execução de um select na tabela T_ALBUM
+def selectAlbums():
+    try:
+        conn = getConnection()
+        cursor = conn.cursor()
+        sql_select = 'SELECT * FROM T_ALBUM'
+        cursor.execute(sql_select)
+        i = 1  
+        for result in cursor: 
+            print(f'\nlinha {i}: {result}')
+            i += 1
+    except Exception as e:
+        print(f'Erro ao executar select: {e}')
+
+#Execução de um select na tabela T_MUSIC
+def selectMusics():
+    try:
+        conn = getConnection()
+        cursor = conn.cursor()
+        sql_select = 'SELECT * FROM T_MUSIC'
+        cursor.execute(sql_select)
+        i = 1  
+        for result in cursor: 
+            print(f'\nlinha {i}: {result}')
+            i += 1
+    except Exception as e:
+        print(f'Erro ao executar select: {e}')
+
+#Inserção de um artista na tabela T_ARTIST
+def insertArtist():
+    try:
+        conn = getConnection()
+        cursor = conn.cursor()
+        sql_insert = f"INSERT INTO T_ARTIST VALUES ('fw56htg84tyi', 'BTS', 'artist.png', 9085)"
+        cursor.execute(sql_insert)
+        conn.commit()
+        print('\nDados inseridos com sucesso!\n')
+    except Exception as e:
+        print(f'Erro ao executar insert: {e}')
+
+#Inserção de um álbum na tabela T_ALBUM
+def insertAlbum():
+    try:
+        conn = getConnection()
+        cursor = conn.cursor()
+        sql_insert = "INSERT INTO T_ALBUM VALUES ('ws59loy92dv', 'op5thbd9362ds', 'Born Pink', 'album.png')"
+        cursor.execute(sql_insert)
+        conn.commit()
+        print('\nDados inseridos com sucesso!\n')
+    except Exception as e:
+        print(f'Erro ao executar insert: {e}')
+
+#Inserção de uma música na tabela T_MUSIC
+def insertMusic():
+    try:
+        conn = getConnection()
+        cursor = conn.cursor()
+        sql_insert = "INSERT INTO T_MUSIC VALUES ('2w5d6pyzx63', '312plm84sde', 'Imagine', 10800)"
+        cursor.execute(sql_insert)
+        conn.commit()
+        print('\nDados inseridos com sucesso!\n')
+    except Exception as e:
+        print(f'Erro ao executar insert: {e}')
+
+
+#Fechando a conexão com o banco de dados
+def closeConnection(conn):
+    try:
+        conn.close()
+        print('\nConexão fechada com sucesso!')
+    except Exception as e:
+        print(f'Erro ao fechar conexão: {e}')
 
 @app.route('/index')
 def pagina1():
@@ -115,3 +211,4 @@ def pagina1():
 #Principal
 if __name__ == "__main__":
     app.run(debug=True)
+    conn = getConnection()
